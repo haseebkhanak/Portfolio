4a30e9eadd4070dc442783a2d6fb0817f10dbfa8
haseebkhanak <saadkhanak13@gmail.com>
2024-12-29 19:13:33 +0500

    Files Added

--- src/portfolio.jsx
+++ src/portfolio.jsx
@@ -23,6 +23,7 @@ import fb from './images/fb.PNG';
 import git from './images/git.PNG';
 import ln from './images/ln.PNG';
 import wa from './images/wa.PNG';
+import cross from './images/cross.PNG';
 import OrbitingCircles from './magicui.jsx/orbiting';
 import { TypingAnimation } from './magicui.jsx/typeanimate';
 import { BorderBeam } from './magicui.jsx/borderbeam';
@@ -32,10 +33,10 @@ import classNames from 'classnames';
 import { AnimatedList } from './magicui.jsx/animatelist';
 import { useRef, useEffect, useState } from 'react';
 export default function MainPage(className) {
-  const frontendRef = useRef('')
-  const backendendRef = useRef('')
-  const fullstackRef = useRef('')
-  const blurRef=useRef('')
+  const frontendRef = useRef(null)
+  const backendendRef = useRef(null)
+  const fullstackRef = useRef(null)
+  const blurRef=useRef(null)
 
   const viewFrontend = () => {
     frontendRef.current.style.display = "block"
@@ -74,6 +75,14 @@ export default function MainPage(className) {
     'MERN stack implemented for web app integrating both frontend and backend with database and for model python is used and flask is used on backend',
     'For frontend HTML, CSS, JavaScript bootstrap and for backend Php and MySQLi have been implemented',
     'Python is used for ML and Regressors are used to train a model']
+
+  const links=['https://github.com/haseebkhanak/GPA-Calculator',
+  'https://github.com/haseebkhanak/Weather-Forecast',
+  'https://github.com/haseebkhanak/Medi-Assist',
+  'https://github.com/haseebkhanak/MedicineRecommendation',
+  'https://github.com/haseebkhanak/Mind-Finder',
+  'https://github.com/haseebkhanak/PlayersPrediction'
+  ]
   const next = () => {
 
     if (count >= images.length - 1) {
@@ -137,6 +146,11 @@ export default function MainPage(className) {
 
   const Notification = ({ img }) => {
     return (
+      <>
+        {/* <div className="blur" ref={blurRef}>
+
+</div> */}
+      <div>
       <figure
         className={classNames(
           "relative mx-auto min-h-fit w-full max-w-[200px] cursor-pointer overflow-hidden rounded-2xl p-4",
@@ -147,12 +161,14 @@ export default function MainPage(className) {
         <div>
 
           <div className='w-[350] shadow-2xl relative rounded-lg'>
-            <BorderBeam size={250} duration={5} />
+            {/* <BorderBeam size={250} duration={5} /> */}
             <img src={img} alt="No" />
           </div>
 
         </div>
       </figure>
+      </div>
+      </>
     );
   };
 
@@ -168,71 +184,17 @@ export default function MainPage(className) {
   const github=()=>{
     window.location.assign("https://github.com/haseebkhanak/haseebkhanak.git")
   }
-  
-  // const Icons = {
-  //     // gitHub: () => (
-  //     //   <svg width="50" height="50" viewBox="0 0 438.549 438.549">
-  //     //     <path
-  //     //       fill="currentColor"
-  //     //       d="M409.132 114.573c-19.608-33.596-46.205-60.194-79.798-79.8-33.598-19.607-70.277-29.408-110.063-29.408-39.781 0-76.472 9.804-110.063 29.408-33.596 19.605-60.192 46.204-79.8 79.8C9.803 148.168 0 184.854 0 224.63c0 47.78 13.94 90.745 41.827 128.906 27.884 38.164 63.906 64.572 108.063 79.227 5.14.954 8.945.283 11.419-1.996 2.475-2.282 3.711-5.14 3.711-8.562 0-.571-.049-5.708-.144-15.417a2549.81 2549.81 0 01-.144-25.406l-6.567 1.136c-4.187.767-9.469 1.092-15.846 1-6.374-.089-12.991-.757-19.842-1.999-6.854-1.231-13.229-4.086-19.13-8.559-5.898-4.473-10.085-10.328-12.56-17.556l-2.855-6.57c-1.903-4.374-4.899-9.233-8.992-14.559-4.093-5.331-8.232-8.945-12.419-10.848l-1.999-1.431c-1.332-.951-2.568-2.098-3.711-3.429-1.142-1.331-1.997-2.663-2.568-3.997-.572-1.335-.098-2.43 1.427-3.289 1.525-.859 4.281-1.276 8.28-1.276l5.708.853c3.807.763 8.516 3.042 14.133 6.851 5.614 3.806 10.229 8.754 13.846 14.842 4.38 7.806 9.657 13.754 15.846 17.847 6.184 4.093 12.419 6.136 18.699 6.136 6.28 0 11.704-.476 16.274-1.423 4.565-.952 8.848-2.383 12.847-4.285 1.713-12.758 6.377-22.559 13.988-29.41-10.848-1.14-20.601-2.857-29.264-5.14-8.658-2.286-17.605-5.996-26.835-11.14-9.235-5.137-16.896-11.516-22.985-19.126-6.09-7.614-11.088-17.61-14.987-29.979-3.901-12.374-5.852-26.648-5.852-42.826 0-23.035 7.52-42.637 22.557-58.817-7.044-17.318-6.379-36.732 1.997-58.24 5.52-1.715 13.706-.428 24.554 3.853 10.85 4.283 18.794 7.952 23.84 10.994 5.046 3.041 9.089 5.618 12.135 7.708 17.705-4.947 35.976-7.421 54.818-7.421s37.117 2.474 54.823 7.421l10.849-6.849c7.419-4.57 16.18-8.758 26.262-12.565 10.088-3.805 17.802-4.853 23.134-3.138 8.562 21.509 9.325 40.922 2.279 58.24 15.036 16.18 22.559 35.787 22.559 58.817 0 16.178-1.958 30.497-5.853 42.966-3.9 12.471-8.941 22.457-15.125 29.979-6.191 7.521-13.901 13.85-23.131 18.986-9.232 5.14-18.182 8.85-26.84 11.136-8.662 2.286-18.415 4.004-29.263 5.146 9.894 8.562 14.842 22.077 14.842 40.539v60.237c0 3.422 1.19 6.279 3.572 8.562 2.379 2.279 6.136 2.95 11.276 1.995 44.163-14.653 80.185-41.062 108.068-79.226 27.88-38.161 41.825-81.126 41.825-128.906-.01-39.771-9.818-76.454-29.414-110.049z"
-  //     //     />
-  //     //   </svg>
-  //     // ),
-  //     ex: () => (
-  //         <img
-  //         src={ex}  
-  //         alt="js"
-  //         width="50"
-  //         height="50"
-  //       />
-  //     ),
-  //     js: () => (
-  //         <img
-  //         src={js}  
-  //         alt="js"
-  //         width="50"
-  //         height="50"
-  //       />
-  //     ),
-  //     react: () => (
-  //         <img
-  //           src={react}  
-  //           alt="react"
-  //           width="50"
-  //           height="50"
-  //         />
-  //       ),
 
-  //     html: () => (
-  //         <img
-  //         src={html}  
-  //         alt="html"
-  //         width="50"
-  //         height="50"
-  //       />
-  //     ),
-  //     mondb: () => (
-  //         <img
-  //         src={mondb}  
-  //         alt="mondb"
-  //         width="50"
-  //         height="50"
-  //       />
-  //     ),
-  //   };
   return (
 
     <>
-      {/* <div className="blur" ref={blurRef}>
 
-      </div> */}
+      <div ref={bodyRef} className="circle-hesitate overflow-hidden">
 
-      <div ref={bodyRef} className="circle-hesitate">
-
-        <nav className="bg-white space-x-4 flex shadow-2xl navbar">
+        <nav className="bg-white space-x-4 flex shadow-2xl navbar overflow-hidden">
           <img src={Logo} alt="Error" className='logo ml-10 mt-2 mb-2' />
           <div className='pl-80 lg:space-x-20 lg:mt-5 sm:mt-2 items sm:space-x-10'>
-            <a href='#about' className='aboutbtn'>About</a >
+            {/* <a href='#about' className='aboutbtn'>About</a > */}
             <a href='#skills' className='skillsbtn'>Skills</a>
             <a href='#projects' className='projectsbtn'>Projects</a>
             <a href='#services' className='skillsbtn'>Services</a>
@@ -247,62 +209,15 @@ export default function MainPage(className) {
             <img src={react} alt="react" />
           </div>
         </div>
-        {/* <div className="circle-mondb">
-    <img src={mondb} alt="mondb" />
-  </div> 
-  <div className="circle-js">
-    <img src={js} alt="JavaScript" />
-  </div> */}
-        {/* <div className="flex h-[500px] flex-col items-center justify-center" >
-      <span>
-      <img src={profile} alt="Error" style={{width:"300px",height:'300px'}}/>
-      </span>
-
-      <OrbitingCircles
-        className="size-[30px] border-none bg-transparent"
-        duration={20}
-        delay={20}
-        radius={100}
-      >
-        <Icons.mondb />
-      </OrbitingCircles>
-      <OrbitingCircles
-        className="size-[30px] border-none bg-transparent"
-        duration={20}
-        // delay={10}
-        radius={100}
-      >
-        <Icons.react />
-      </OrbitingCircles>
-
-      <OrbitingCircles
-        className="size-[50px] border-none bg-transparent"
-        radius={180}
-        duration={20}
-        reverse
-      >
-        <Icons.js />
-      </OrbitingCircles>
-      <OrbitingCircles
-        className="size-[50px] border-none bg-transparent"
-        radius={180}
-        duration={20}
-        delay={20}
-        reverse
-      >
-        <Icons.html />
-      </OrbitingCircles>
-    </div> */}
-
 
         <img src={profile} alt="Error" className='personal' />
         <div>
-          <div className='bg-white shadow-xl ml-20 mt-20 relative rounded-lg about' id='about'>
-            <BorderBeam size={250} duration={5} />
-            <p className='aboutmetext text-5xl font-bold ml-20 text-transparent 
+            <p className='aboutmetext text-5xl font-bold text-transparent 
                      bg-gradient-to-r from-green-500 via-purple-500
-                     to-indigo-500 bg-clip-text pt-5'>About Me</p>
-            <p className='nametext'></p>
+                     to-indigo-500 bg-clip-text pt-20'>About Me</p>
+          <div className='bg-white shadow-xl ml-20 relative rounded-lg about' id='about'>
+            {/* <BorderBeam size={250} duration={5} /> */}
+            <p className='nametext pt-5'></p>
             <p className='desctext text-transparent 
                      bg-gradient-to-r from-green-500 via-purple-500
                      to-indigo-500 bg-clip-text ml-20'>Software Engineer</p>
@@ -310,7 +225,7 @@ export default function MainPage(className) {
 
               <p className='detailtext'>I am a passionate and skilled full-stack <br /> developer specializing in the MERN stack and  <br /> machine learning. With expertise in building <br /> scalable web applications.</p>
             </div>
-            <a href="/Resume.pdf" download="Resume.pdf" className='downloadbtn px-2 py-2 group relative inline-block overflow-hidden bg-blue-300 border border-blue-500 focus:outline-none focus:ring'>  <span
+            <a href="/Portfolio/Resume.pdf" download="Resume.pdf" className='downloadbtn px-2 py-2 group relative inline-block overflow-hidden bg-blue-300 border border-blue-500 focus:outline-none focus:ring'>  <span
     className="absolute inset-y-0 left-0 w-[2px] bg-blue-700 border border-blue-800 transition-all group-hover:w-full"
   ></span>
 
@@ -319,8 +234,8 @@ export default function MainPage(className) {
   >
     Download Resume
   </span>
-</a> <br />
-            {/* <a className='downloadbtn bg-blue-300 border border-blue-500 px-2 py-2 hover hover:bg-blue-800 hover:text-white hover:border-blue-900' href="/Resume.pdf" download="Resume.pdf">Download Resume</a> <br /><br /> */}
+</a> <br /> <br />
+
           </div>
         </div>
 
@@ -346,8 +261,8 @@ export default function MainPage(className) {
           <p className='projects mt-10 font-bold text-5xl text-transparent 
                      bg-gradient-to-r from-green-500 via-purple-500
                      to-indigo-500 bg-clip-text'>Projects</p> <br />
-
-          <div className='projectslides flex bg-white shadow-xl ml-20 mt-30 relative rounded-lg'>
+<div className='projectslides bg-white shadow-xl ml-20 mt-30 pb-10 relative rounded-lg'>
+          <div className='slides flex'>
             <img src={images[count]} alt="no" className='images py-8' />
             <p className='overviewtext ml-10 py-8 pr-5'> <span className='text-2xl font-bold text-transparent 
                      bg-gradient-to-r from-blue-500 via-purple-500
@@ -355,16 +270,18 @@ export default function MainPage(className) {
                      bg-gradient-to-r from-blue-500 via-purple-500
                      to-indigo-500 bg-clip-text'>Technologies</span> <br />{tech[count]}</p>
           </div> <br />
+          <a className='repo bg-black px-20 py-2 mb-20 text-white' href={links[count]} target='_blank'>Visit Repo</a>
+          </div> <br />
           <div className='flex'>
             <img src={prevbtn} onClick={previous} alt="" className='prev' />
             <img src={nextbtn} onClick={next} alt="" className='next' />
           </div> <br /><br />
         </div>
 
-        <div className='viewfrontend bg-white shadow-xl rounded-lg absolute' ref={frontendRef}>
-          <p className='viewfrontendtext text-2xl ml-5 mt-5'>Frontend Development</p> <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5} className="cross size-6">
-            <path d="M6 18 18 6M6 6l12 12" style={{ cursor: "pointer" }} onClick={crossSvg} />
-          </svg></span>
+        <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-lg z-40 blur" ref={blurRef}></div>
+        <div className='viewfrontend absolute bg-white rounded-lg z-50' ref={frontendRef}>
+          <p className='viewfrontendtext text-2xl ml-5 mt-5'>Frontend Development</p>
+          <img src={cross} alt="No" className='cross' onClick={crossSvg}/>
           <p className='ml-5 mt-5'>✔️ Building the user interface using HTML, CSS, and JavaScript.</p>
           <p className='ml-5'>✔️ Implementing frameworks like React, Bootstrap and Tailwind.</p>
           <p className='ml-5'>✔️ Transforming UI/UX design into functional and responsive applications.</p>
@@ -372,10 +289,10 @@ export default function MainPage(className) {
 
         </div>
 
-        <div className='viewbackend bg-white shadow-xl rounded-lg absolute' ref={backendendRef}>
-          <p className='viewbackendtext text-2xl ml-5 mt-5'>Backend Development</p> <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5} className="cross size-6">
-            <path d="M6 18 18 6M6 6l12 12" style={{ cursor: "pointer" }} onClick={crossSvg} />
-          </svg></span>
+        {/* <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-lg z-40 blur" ref={blurRef}></div> */}
+        <div className='viewbackend bg-white shadow-xl rounded-lg absolute z-50' ref={backendendRef}>
+          <p className='viewbackendtext text-2xl ml-5 mt-5'>Backend Development</p>
+          <img src={cross} alt="No" className='cross' onClick={crossSvg}/>
           <p className='ml-5 mt-5'>✔️ Building and managing server-side logic using languages like Node.js, PHP.</p>
           <p className='ml-5 mt-3'>✔️ Implementing frameworks like ExpressJs.</p>
           <p className='ml-5 mt-3'>✔️ Designing, implementing, and maintaining databases i.e, MySQL, MongoDB.</p>
@@ -384,10 +301,9 @@ export default function MainPage(className) {
 
         </div>
 
-        <div className='viewfullstack bg-white shadow-xl rounded-lg absolute' ref={fullstackRef}>
-          <p className='viewfullstacktext text-2xl ml-5 mt-5'>Fullstack Development</p> <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.5} className="cross size-6">
-            <path d="M6 18 18 6M6 6l12 12" style={{ cursor: "pointer" }} onClick={crossSvg} />
-          </svg></span>
+        <div className='viewfullstack bg-white shadow-xl rounded-lg absolute z-50' ref={fullstackRef}>
+          <p className='viewfullstacktext text-2xl ml-5 mt-5'>Fullstack Development</p>
+          <img src={cross} alt="No" className='cross' onClick={crossSvg}/>
           <p className='ml-5 mt-5'>✔️ Designing responsive user interfaces and Developing server-side logic.</p>
           <p className='ml-5 mt-3'>✔️ Connecting frontend and backend through APIs.</p>
           <p className='ml-5 mt-3'>✔️ Building and deploying complete web applications from scratch.</p>
@@ -421,9 +337,7 @@ export default function MainPage(className) {
                 <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
               </svg></span>
             </div>
-            {/* <div className='ml bg-white shadow-xl rounded-lg'>
-                        <p className='mt-5 text-red-800 text-2xl text-bold'>Machine Learning</p>
-                      </div> */}
+
           </div> <br />
         </div> <br /><br />
         
@@ -481,16 +395,7 @@ export default function MainPage(className) {
     Services
   </span>
 </a>
-<a href='#contact' className='contactfooter px-2 group relative inline-block overflow-hidden border border-black focus:outline-none focus:ring'>  <span
-    className="absolute inset-y-0 left-0 w-[2px] bg-black transition-all group-hover:w-full group-active:bg-black"
-  ></span>
 
-  <span
-    className="relative text-black transition-colors group-hover:text-white"
-  >
-    Contact Me
-  </span>
-</a>
           </div>
         </div>
 </div>
